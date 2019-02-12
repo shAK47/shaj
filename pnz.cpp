142b58524c6d7ca7d346e94054f611699ea9492c
shAK47 <47551957+shAK47@users.noreply.github.com>
2019-02-12 12:22:34 +0530

    Create pnz.cpp

---
+++ pnz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int s;
+	cin>>s;
+	if(s>0)
+	cout<<"Positive";
+	else if(s<0)
+	cout<<"Negative";
+	else
+	cout<<"Zero";
+	return 0;
+}
