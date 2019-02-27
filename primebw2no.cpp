43c6503d29e98279c4e67b771c00ebd2a534c97d
shAK47 <47551957+shAK47@users.noreply.github.com>
2019-02-27 22:53:01 +0530

    Create primebw2no.cpp

---
+++ primebw2no.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n1,n2,count;
+cin>>n1>>n2;
+for(int i=n1+1;i<n2;i++)
+{
+	count=0;
+	for(int j=2;j<i;j++)
+	{
+		if(i%j==0)
+		count++;
+	}
+	if(count==0)
+	cout<<i<<"";
+}
+return 0;
+}
