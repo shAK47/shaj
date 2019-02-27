dd4131d2d333af64c12cae07c679ca30489ce351
shAK47 <47551957+shAK47@users.noreply.github.com>
2019-02-27 20:34:59 +0530

    Update nk.cpp

--- nk.cpp
+++ nk.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-int i,n,k,sum=0;
+int i,n,a[10],k,sum=0;
 cin>>n;
 cin>>k;
-for(i=1;i<=n;i++)
-   cout<<i<<" ";
-for(i=1;i<=k;i++)
-   sum=sum+i;
-cout<<"\n"<<sum;
+for(i=0;i<n;i++)
+   cin>>a[i];
+for(i=0;i<k;i++)
+   sum=sum+a[i];
+cout<<sum;
 return 0;
 }
