9cb3729e4c4e1c9a38613b85ef7f278017159d2b
shAK47 <47551957+shAK47@users.noreply.github.com>
2019-03-01 09:06:51 -0800

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int i,j,n,a[10],temp;
+cin>>n;
+for(i=0;i<n;i++)
+cin>>a[i];
+for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+}
+for(i=0;i<n;i++)
+cout<<a[i]<<" ";
+return 0;
+}
