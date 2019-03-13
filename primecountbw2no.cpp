fbfd04e62fddbc43453e596c52109868c9a3993e
shAK47 <47551957+shAK47@users.noreply.github.com>
2019-03-13 07:05:36 +0530

    Update primecountbw2no.cpp

--- primecountbw2no.cpp
+++ primecountbw2no.cpp
@@ -9,13 +9,20 @@ for(i=start;i<=end;i++)
 {
 	if(i==2)
 		count++;
+	else
+	{
 	for(j=2;j<i;j++)
 	{
+		c=0;
 		if(i%j==0)
+    	{	
     		c++;
+    		break;
+    	}
 	}
 	if(c==0)
 		count++;
+	}
 }
 cout<<count;
 return 0;
